45a5d0bef6f1e1775d6ace607a1da48e3beeb112
Setha Va <vasetha1@gmail.com>
2023-11-02 20:20:53 +0700

    (style) improve ui

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -5,36 +5,32 @@ import { BillboardClient } from "./components/client";
 import { Suspense } from "react";
 import Loading from "./loading";
 
-const BillBoardsPage = async ({
-    params
-}: {params: {storeId: string}}) => {
-    const billboards = await prismadb.billboard.findMany({
-        where: {
-            storeId: params.storeId
-        },
-        orderBy: {
-            createdAt: "desc"
-        }
+const BillBoardsPage = async ({ params }: { params: { storeId: string } }) => {
+  const billboards = await prismadb.billboard.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedBillboards: BillboardColumn[] = billboards.map(
+    (item: any) => ({
+      id: item.id,
+      label: item.label,
+      createdAt: format(item.createdAt, "MMMM dd, yyyy"),
     })
-
-
-
-    const formattedBillboards: BillboardColumn[] = billboards.map((item: any) => (
-        {
-            id: item.id,
-            label: item.label,
-            createdAt: format(item.createdAt, "MMMM dd, yyyy")
-        }
-    ))
-    return (
-        <Suspense fallback={<Loading />}>
-            <div className="flex-col">
-                <div className="flex-1 space-y-4 p-8 pt-6 ">
-                    <BillboardClient data={formattedBillboards}/>
-                </div>
-            </div>
-        </Suspense>
-    )
-}
+  );
+  return (
+    <Suspense fallback={<Loading />}>
+      <div className="flex-col">
+        <div className="flex-1 space-y-4 p-8 pt-6 ">
+          <BillboardClient data={formattedBillboards} />
+        </div>
+      </div>
+    </Suspense>
+  );
+};
 
 export default BillBoardsPage;
